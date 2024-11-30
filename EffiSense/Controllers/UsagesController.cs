0979a02a4db653f32e4b927f6fffdb683f9d2351
dist <120486581+dimitarpst@users.noreply.github.com>
2024-11-30 03:51:15 +0200

    -added AJAX on usage

--- EffiSense/Controllers/UsagesController.cs
+++ EffiSense/Controllers/UsagesController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -60,10 +61,13 @@ namespace EffiSense.Controllers
         {
             var user = _userManager.GetUserAsync(User).Result;
 
-            ViewData["UserId"] = user.Id; 
-            ViewData["ApplianceId"] = new SelectList(_context.Appliances.Where(a => a.Home.UserId == user.Id), "ApplianceId", "Name");
+            ViewData["HomeId"] = new SelectList(_context.Homes.Where(h => h.UserId == user.Id), "HomeId", "Size");
+
+            ViewData["ApplianceId"] = new SelectList(Enumerable.Empty<SelectListItem>());
+
             return View();
         }
+
 
         // POST: Usages/Create
         [HttpPost]
@@ -101,7 +105,6 @@ namespace EffiSense.Controllers
 
 
 
-        // GET: Usages/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -109,23 +112,29 @@ namespace EffiSense.Controllers
                 return NotFound();
             }
 
-            var usage = await _context.Usages.FindAsync(id);
+            var usage = await _context.Usages
+                .Include(u => u.Appliance)
+                .ThenInclude(a => a.Home)
+                .FirstOrDefaultAsync(u => u.UsageId == id);
+
             if (usage == null)
             {
                 return NotFound();
             }
 
             var user = await _userManager.GetUserAsync(User);
-
             if (usage.UserId != user.Id)
             {
-                return Forbid(); 
+                return Forbid();
             }
 
-            ViewData["UserId"] = usage.UserId;
-            ViewData["ApplianceId"] = new SelectList(_context.Appliances.Where(a => a.Home.UserId == user.Id), "ApplianceId", "Name", usage.ApplianceId);
+            ViewData["SelectedHomeId"] = usage.Appliance.HomeId;
+            ViewData["HomeId"] = new SelectList(_context.Homes.Where(h => h.UserId == user.Id), "HomeId", "Size", usage.Appliance.HomeId);
+            ViewData["SelectedApplianceId"] = usage.ApplianceId; 
             return View(usage);
         }
+
+
 
         // POST: Usages/Edit/5
         [HttpPost]
@@ -220,6 +229,24 @@ namespace EffiSense.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAppliancesByHome(int homeId)
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            var appliances = await _context.Appliances
+                .Where(a => a.HomeId == homeId && a.Home.UserId == user.Id)
+                .Select(a => new
+                {
+                    a.ApplianceId,
+                    a.Name
+                })
+                .ToListAsync();
+
+            return Json(appliances);
+        }
+
 
         private bool UsageExists(int id)
         {

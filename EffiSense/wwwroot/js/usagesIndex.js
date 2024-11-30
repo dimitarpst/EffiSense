0d8d0dd676822287dc7fe8d383db44a729b7f31c
dist <120486581+dimitarpst@users.noreply.github.com>
2024-11-30 11:18:25 +0200

    -added date filter

---
+++ EffiSense/wwwroot/js/usagesIndex.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    //Date Filtering on Index
+    $("#filterDate").change(function () {
+        const selectedDate = $(this).val();
+        if (selectedDate) {
+            localStorage.setItem("selectedDate", selectedDate);
+        }
+        $.ajax({
+            url: '/Usages/FilterByDate',
+            data: { date: selectedDate },
+            success: function (data) {
+                $("#usageTableBody").html(data);
+            },
+            error: function () {
+                alert("Error fetching data.");
+            }
+        });
+    });
+});

9b16cb793973518bab2a3ace2c57e4d939b376f2
dist <120486581+dimitarpst@users.noreply.github.com>
2024-11-30 02:07:26 +0200

    -added usages

---
+++ EffiSense/Models/Usage.cs
@@ -0,0 +1,18 @@
+﻿    public class Usage
+    {
+        public int UsageId { get; set; }  
+
+        public string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+        public int ApplianceId { get; set; }
+        public virtual Appliance Appliance { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public double EnergyUsed { get; set; }
+
+        public int UsageFrequency { get; set; }
+
+        public double Duration { get; set; } 
+    }

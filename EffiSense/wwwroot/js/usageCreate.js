7b40a73d2c09d845b8da2c50c65cb60aa6b9b1aa
dist <120486581+dimitarpst@users.noreply.github.com>
2024-11-30 11:18:25 +0200

    -added date filter

---
+++ EffiSense/wwwroot/js/usageCreate.js
@@ -0,0 +1,37 @@
+﻿$(document).ready(function () {
+    const selectedDate = localStorage.getItem("selectedDate");
+    const usageDateInput = $("#usageDate");
+
+    if (selectedDate) {
+        usageDateInput.val(selectedDate);
+    }
+
+    $("#createUsageForm").on("submit", function (e) {
+        if (!usageDateInput.val()) {
+            alert("Please select a date before creating a usage.");
+            e.preventDefault();
+        }
+    });
+
+    $("#HomeId").change(function () {
+        var homeId = $(this).val();
+        var applianceDropdown = $("#ApplianceId");
+
+        applianceDropdown.empty().append('<option value="">Select Appliance</option>');
+
+        if (homeId) {
+            $.ajax({
+                url: '/Usages/GetAppliancesByHome',
+                data: { homeId: homeId },
+                success: function (data) {
+                    $.each(data, function (index, appliance) {
+                        applianceDropdown.append('<option value="' + appliance.applianceId + '">' + appliance.name + '</option>');
+                    });
+                },
+                error: function () {
+                    alert("Error fetching appliances.");
+                }
+            });
+        }
+    });
+});
